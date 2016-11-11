31862973e5628753c5e05725c0183ee83d6618bc
jprieto9 <javier.prieto.cepeda@gmail.com>
2016-11-11 11:00:30 +0000

    Funcionando jsp con listas para mostrar varios mensajes

--- TIW_P1/src/handlers/ChatRequestHandler.java
+++ TIW_P1/src/handlers/ChatRequestHandler.java
@@ -1,5 +1,8 @@
 package handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import jms.InteraccionMQ;
@@ -24,10 +27,11 @@ public class ChatRequestHandler extends ActionHandler {
 		HttpSession sesion = request.getSession(false);
 		String usuarioSession = (String) sesion.getAttribute("userEmailSession");
 		InteraccionMQ mq = new InteraccionMQ();
-		MessageChat mensaje = mq.lecturaMQ(usuarioSession);
-		System.out.println("MENSAJES QUE ESTOY RECIBIENDO : ---" + mensaje);
+		List<MessageChat> listaMensajes = new ArrayList<MessageChat>();
+        listaMensajes = mq.lecturaMQ(usuarioSession);
+		System.out.println("MENSAJES QUE ESTOY RECIBIENDO : ---" + listaMensajes.size());
 		/*Aquí debemos de leer de la cola de JMS para enviarle al form los mensajes recibidos*/
-		request.setAttribute("mensajeRecibido", mensaje);
+		request.setAttribute("mensajesRecibidos", listaMensajes);
 	}
 
 }
